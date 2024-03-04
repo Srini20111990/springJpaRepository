00c08264a6c8421369a74e8152b81dcfde12786e
srini20111990 <pothulasrinivvas@gmail.com>
2024-03-04 08:22:34 +0530

    student class added

---
+++ src/main/java/com/srini/dao/Student.java
@@ -0,0 +1,22 @@
+package com.srini.dao;
+
+public class Student {
+	
+	private Integer stuId;
+	private String stuName;
+	public Integer getStuId() {
+		return stuId;
+	}
+	public void setStuId(Integer stuId) {
+		this.stuId = stuId;
+	}
+	public String getStuName() {
+		return stuName;
+	}
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+	
+	
+
+}
